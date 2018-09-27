5e34c9af9be75f77d7e80ab391b9b66b66e81dd7
Jonathanaparicio <jonathanaparicio@u.boisestate.edu>
2018-09-26 20:06:55 -0600

    nav bar

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+  <head>
+    <title>CADAVentures - Rentals</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+  
+  </head>
+  <body>
+    <div class="nav-bar" id="nav-bar">
+      <ul >
+        <li class="Home" id+"current page">
+          <a href="index.php"> Home</a>
+        </li>
+        <li class="Residential"><a>Residential</a></li>
+        <li class="Commercial"><a>Commercial</a></li>
+        <li class="Industrial"><a>Industrial</a></li>
+      </ul >
+    </div>
+    <div class="content">
+      <div class="info">
+        <ul>
+          <li class="contact-us"><b>Contact Us</b></li>
+          <li class="main-req"><b>Maintenance Request</b></li>
+          <li class="Pay"><b>Pay Online</b></li>
+        </ul>
+      </div>
+
+    <div id="footer">
+      <li class="first">@Jonathan Aparicio</li>
+    </div>
+  </body>
+</html>
