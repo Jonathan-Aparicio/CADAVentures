2ac94306308a942c146c60287ec6c0a182f2cc30
jonathan-aparicio <jonathandaparicio@gmail.com>
2018-11-06 10:24:50 -0700

    2 try at if link

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
         <ul>
           <li class="contact-us"><a href="Contact.php">Contact Us</a></li>
           <li class="main-req">
-            <a href="<<?php if (isset($_SESSION["access_granted"]) && $_SESSION["access_granted"]) {
+            <a href="<?php if (isset($_SESSION["access_granted"]) && $_SESSION["access_granted"]) {
               echo "Maintence.php";
             }else{
               echo "log-in.php";
