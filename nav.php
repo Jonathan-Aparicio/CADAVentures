3f0961fcc00d9cbab5c461215afe4e86d5795277
jonathan-aparicio <jonathandaparicio@gmail.com>
2018-11-06 09:22:40 -0700

    try at universal nav bar

---
+++ nav.php
@@ -0,0 +1,15 @@
+<body>
+  <div class="banner">
+    <img class="logo" src="logo.png">
+  </div>
+  <div class="nav-bar" id="nav-bar">
+    <ul >
+      <li class="Home" id+"current page">
+        <a href="index.php"> Home</a>
+      </li>
+      <li class="Residential"><a href="Residential.php">Residential</a></li>
+      <li class="Commercial"><a href="Commercial.php">Commercial</a></li>
+      <li class="Industrial"><a href="Industrial.php">Industrial</a></li>
+    </ul >
+  </div>
+</bod>
