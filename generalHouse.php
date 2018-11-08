699a7eef42f8b3d0daf09fd65e263e293df3773d
jonathan-aparicio <jonathandaparicio@gmail.com>
2018-11-07 19:55:36 -0700

    try at general house 14

--- generalHouse.php
+++ generalHouse.php
@@ -2,7 +2,7 @@
   $dao = new Dao();
   $info = array();
 //  $type = $_GET["type"];
-  $id = $_GET["id"];
+//  $id = $_GET["id"];
 //  $info =  $dao->getAllHouseInfo($id, $type);
   echo "got here";
   // if($type == "Residential"){
