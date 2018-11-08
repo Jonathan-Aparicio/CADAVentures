847b74a99521fb87fca0ec335096674a550212f8
jonathan-aparicio <jonathandaparicio@gmail.com>
2018-11-07 23:41:38 -0700

    general updates 2

--- index.php
+++ index.php
@@ -3,26 +3,12 @@
     <title>CADAVenturs - Rentals</title>
     <link rel="stylesheet" type="text/css" href="index.css"/>
     <link rel="shortcut icon" type="image/ico" href="favicon (1).ico"/>
-  
+
   </head>
   <body>
     <div>
       <?php require_once "nav.php";?>
     </div>
-    <!-- <div class="banner">
-      <img class="logo" src="logo.png">
-    </div>
-    <div class="nav-bar" id="nav-bar">
-      <ul >
-        <li class="Home" id+"current page">
-          <a href="index.php"> Home</a>
-        </li>
-        <li class="Residential"><a href="Residential.php">Residential</a></li>
-        <li class="Commercial"><a href="Commercial.php">Commercial</a></li>
-        <li class="Industrial"><a href="Industrial.php">Industrial</a></li>
-      </ul >
-    </div> -->
-
     <div class="content">
       <div class="info">
         <ul>
@@ -36,7 +22,7 @@
             } ?>"
 
             >Maintenance Request</a></li>
-          <li class="Pay"><a href="log-in.php">Pay Online</a></li>
+          <li class="Pay"><a href="PayApp.php">Pay Online</a></li>
         </ul>
         <hr class="info-line">
       </div>
