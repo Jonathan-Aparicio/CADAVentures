51553f5081994d4f57a0ef05761b49ca24745751
jonathan-aparicio <jonathandaparicio@gmail.com>
2018-11-07 19:32:29 -0700

    try at general house 10

--- Maintence.php
+++ Maintence.php
@@ -1,3 +1,32 @@
-<?php
-session_start();
- echo $_SESSION["message"]; ?>
+<html>
+  <head>
+    <title>CADAVenturs - Rentals</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="shortcut icon" type="image/ico" href="favicon (1).ico"/>
+  </head>
+  <body>
+    <div>
+      <?php require_once "nav.php";?>
+    </div>
+    <div class="content">
+      <div class="info">
+        <ul>
+          <li class="apply"><a href="log-in.php">apply</a></li>
+        </ul>
+        <hr class="info-line">
+      </div>
+      <div>
+        <img class="gen_image" src="<?php echo $info['Photo'] ?>">
+      </div>
+      <div class="address">
+        <p><?php echo $info['StreetAddress']?> <?php echo $info['City']; ?> <?php echo $info['State'] ?></p>
+      </div>
+    </div>
+    <div class="footer">
+      <ul>
+        <li class="footer-text">@Jonathan Aparicio</li>
+        <li class="footer-text"><a href="Contact.php">Contact</a></li>
+      </ul>
+    </div>
+  </body>
+</html>
