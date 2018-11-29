0f48271d44d9d40b4cc762fb1b3bb551f320b568
jonathan-aparicio <jonathandaparicio@gmail.com>
2018-11-28 17:31:15 -0700

    footer at bootm 1

--- Residential.php
+++ Residential.php
@@ -23,11 +23,10 @@ $(document).ready(function () {
           <li><a href="generalHouse.php?type=Residential&id=1">
               <img class="image" src="GreenHouse.jpg" alt="Residential Green House">
             </a></li>
-          <li><a href="generalHouse.php?type=Residential&id=1" >
-            <img class="image" src="GreenHouse.jpg">
-          </a></li>
-          <li> <a href="generalHouse.php?type=Residential&id=2">
-            <img class="image" src="white house.jpg"></a></li>
+          <li><a href="generalHouse.php?type=Residential&id=2">
+            <img class="image-item" src="white house.jpg"></a></li>
+          <li> <a href="generalHouse.php?type=Residential&id=3">
+            <img class="image-item" src="RedHouse.jpg"></a></li>
           </ul>
       </div>
     </div>
